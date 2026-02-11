83c17859a354f8af274173a2820094969e200a09
Roshni1126 <roshiniklbca25@rvu.edu.in>
2026-02-11 11:14:32 +0530

    Add files via upload

---
+++ outputlab1.py
@@ -0,0 +1,36 @@
+Python 3.12.8 (tags/v3.12.8:2dc476b, Dec  3 2024, 19:30:04) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/RVUW247/Desktop/Roshni KL/pythonlab1.py ==========
+Enter a 2
+Enter b79
+Sum of values is 81
+Sub of values is -77
+Mul of values is 158
+Div of values is 0.02531645569620253
+>>> 
+================= RESTART: C:/Users/RVUW247/Desktop/Roshni KL/pythonlab1.py ================
+Enter degree fahrenheit 54
+Enter degree celsius76
+The value of fahrenheit is: 348
+The value of celsius is: 175.55555555555554
+>>> 
+================= RESTART: C:/Users/RVUW247/Desktop/Roshni KL/pythonlab1.py ================
+Enter a:32
+Enter b:7
+Enter c:7
+a is the greatest 32
+>>> 
+================= RESTART: C:/Users/RVUW247/Desktop/Roshni KL/pythonlab1.py ================
+Enter a:24
+Enter b:58i
+Traceback (most recent call last):
+  File "C:/Users/RVUW247/Desktop/Roshni KL/pythonlab1.py", line 30, in <module>
+    b=int(input("Enter b:"))
+ValueError: invalid literal for int() with base 10: '58i'
+>>> 
+================= RESTART: C:/Users/RVUW247/Desktop/Roshni KL/pythonlab1.py ================
+Enter a:46
+Enter b:7
+Enter c:59
+b is the smallest 7
---
+++ pythonlab1.py
@@ -0,0 +1,37 @@
+'''a=int(input("Enter a"))
+b=int(input("Enter b"))
+summ=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+print("Sum of values is",summ)
+print("Sub of values is",sub)
+print("Mul of values is",mul)           
+print("Div of values is",div)'''
+
+'''f=int(input("Enter degree fahrenheit"))
+c=int(input("Enter degree celsius"))
+f=c+272;
+c=(f-32)*5/9;
+print("The value of fahrenheit is:",f)
+print("The value of celsius is:",c)'''
+
+'''a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+if a>b and a>c :
+    print("a is the greatest",a)
+elif b>a and b>c:
+    print("b is the greatest",b)
+else:
+    print("c is the greatest",c)'''
+        
+'''a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+if a<b and a<c :
+    print("a is the smallest",a)
+elif b<a and b<c:
+    print("b is the smallest",b)
+else:
+    print("c is the smallest",c)'''
